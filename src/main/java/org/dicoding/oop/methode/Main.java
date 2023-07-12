9ad0b317c134cafa8a603855a9fac3dd491901b4
meran <amr.bin.an@gmail.com>
2023-07-12 21:07:22 +0700

    oop

---
+++ src/main/java/org/dicoding/oop/methode/Main.java
@@ -0,0 +1,23 @@
+package org.dicoding.oop.methode;
+
+public class Main {
+  public static void main(String[] args) {
+    // Kita tambahkan 1 argumen dengan nilai int 2
+    // Nilai tersebut adalah nilai yang digunakan untuk inisiasi variabel umur
+    Hewan kucing = new Hewan(2);
+    kucing.makan();
+    kucing.jalan();
+    kucing.lari();
+    System.out.println();
+
+    // Contoh penggunaan fungsi getUmur
+
+    System.out.println("Umurnya adalah " + kucing.getUmur());
+    System.out.println();
+
+    // Perhitungan indeks massa tubuh (BMI)
+    // berat(kg) / ( tinggi(m) * tinggi(m) )
+    double bmi = kucing.getBerat() / ((kucing.getTinggi() * 0.01) * (kucing.getTinggi() * 0.01));
+    System.out.println("Indeks massa tubuhnya adalah " + bmi);
+  }
+}
---
+++ src/main/java/org/dicoding/oop/obyek/Main.java
@@ -0,0 +1,18 @@
+package org.dicoding.oop.obyek;
+
+public class Main {
+  public static void main(String[] args) {
+    Hewan Elang = new Hewan("Elang");
+    Hewan Kucing = new Hewan("Kucing");
+
+    Elang.beratHewan(5);
+    Elang.jumlahKakiHewan(2);
+    Elang.cetakHewan();
+
+    System.out.println();
+
+    Kucing.beratHewan(2);
+    Kucing.jumlahKakiHewan(4);
+    Kucing.cetakHewan();
+  }
+}
